648d37585e32185b159974cb97f1a75f5c195b72
abdc06 <abdc2806@gmail.com>
2022-10-22 17:54:08 +0900

    nadak-by-boot 프로젝트 생성

---
+++ nadak-by-boot/src/main/java/com/base/nadakbyboot/NadakByBootApplication.java
@@ -0,0 +1,13 @@
+package com.base.nadakbyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadakByBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NadakByBootApplication.class, args);
+    }
+
+}
